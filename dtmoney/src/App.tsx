90259ebc68c5f2b791e769100834341e4d52b332
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-18 09:30:35 -0300

    Finalizando Transaction Modal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+import { useState } from "react";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionModal } from "./components/TransactionModal";
+
 import { GlobalStyle } from "./styles/global";
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+        },
+      ];
+    });
+  },
+});
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+      <TransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
---
+++ dtmoney/src/components/TransactionModal/index.tsx
@@ -0,0 +1,41 @@
+import Modal from "react-modal";
+import { Container } from "./styles";
+import closeImg from "../../assets/close.svg";
+
+Modal.setAppElement("#root");
+
+interface TransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function TransactionModal({
+  isOpen,
+  onRequestClose,
+}: TransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
